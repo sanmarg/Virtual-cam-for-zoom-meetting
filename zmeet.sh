b1e91c9f8b9597298fc78f835bbc2fa9cbcd6164
Sanmarg Paranjpe <50082154+sanmarg@users.noreply.github.com>
2021-04-05 22:20:58 +0530

    Update zmeet.sh

--- zmeet.sh
+++ zmeet.sh
@@ -1,8 +1,43 @@
 #!/bin/sh
-apt update
+read -p "Do you want to update using apt update RECOMMENDED? Y/N " ANSWER
+case "$ANSWER" in 
+  [yY] | [yY][eE][sS])
+    echo "Updating..."
+    apt update
+    ;;
+  [nN] | [nN][oO])
+    echo "Ok Continuing..."
+    ;;
+  *)
+    echo "Please enter y/yes or n/no"
+    ;;
+esac
+
+clear
+echo "Installing required toolset, Please Wait..."
 apt install v4l2loopback-dkms
 apt install v4l-utils -y
+apt install ffmpeg
 modprobe v4l2loopback
-wget https://zoom.us/client/latest/zoom_amd64.deb
-apt install ./zoom_amd64.deb -y
-ffmpeg -stream_loop -1 -re -i input.mp4 -f v4l2 /dev/video1
+cls
+
+read -p "Do you want to install Zoom Application? Y/N " ANS
+case "$ANS" in 
+  [yY] | [yY][eE][sS])
+    echo "Downloading Zoom and Installing..."
+    wget https://zoom.us/client/latest/zoom_amd64.deb
+    apt install ./zoom_amd64.deb -y
+    cls
+    ;;
+  [nN] | [nN][oO])
+    echo "Ok Continuing further..."
+    cls
+    ;;
+  *)
+    echo "Please enter y/yes or n/no"
+    ;;
+esac
+
+echo "Setting up your virtual camera"
+read -p "Enter the video file location separated by / like /home/demo/input.mp4: " FILE
+ffmpeg -stream_loop -1 -re -i $FILE -f v4l2 /dev/video1
